8738c6aee3cea8ebb7616a2b24b10c52068a8937
alexbarretoz <alexbarreto@gmail.com>
2022-11-06 12:01:13 -0500

    funcion buscar por Editorial y Genero

--- tarea1.py
+++ tarea1.py
@@ -20,3 +20,43 @@ def buscarlibro_autor(valor):
                 print(datos[numero])
             numero= numero + 1
         f.close()
+
+
+#BUSCAR LIBRO POR EDITORIAL
+def buscarlibro_editorial(valor):
+    numero= 0
+    editorial= []
+
+    with open ("libros.csv", encoding= "utf-8") as f:
+        archivo = csv.reader(f)
+        datos= []
+        next(archivo) # hace que el objeto iterador se mueva 1 ( no se ve el titulo)
+    
+        for lista in archivo:
+            editorial.append(lista[4])
+            datos.append(lista)
+        
+            if editorial[numero] == valor:
+                print(datos[numero])
+            numero= numero + 1
+        f.close()
+        
+
+#BUSCAR LIBRO POR GENERO
+def buscarlibro_genero(valor):
+    numero= 0
+    genero= []
+
+    with open ("libros.csv", encoding= "utf-8") as f:
+        archivo = csv.reader(f)
+        datos= []
+        next(archivo) # hace que el objeto iterador se mueva 1 ( no se ve el titulo)
+    
+        for lista in archivo:
+            genero.append(lista[2])
+            datos.append(lista)
+        
+            if genero[numero] == valor:
+                print(datos[numero])
+            numero= numero + 1
+        f.close()
